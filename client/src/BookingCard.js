d821bfa7fa0fd35624b3d12c73723b0a538d80e4
jomonty <100322748+jomonty@users.noreply.github.com>
2023-01-18 13:45:24 +0000

    formatting

--- client/src/BookingCard.js
+++ client/src/BookingCard.js
@@ -2,21 +2,22 @@ import { deleteBooking } from './BookingService';
 
 const BookingCard = ({booking, removeBooking}) => {
 
-    const handleDelete = () => {
-        deleteBooking(booking._id)
-            .then(() => {
-                removeBooking(booking._id)
-            })
-    }
-    return (
-        <>
-        <p>Customer name: {booking.name}</p>
-        <p>Customer email: {booking.email}</p>
-        <p>Checked in? {booking.checkedIn}</p>
-        <button onClick={() => handleDelete()}> 🗑 </button>
-        <hr />
-        </>
-    )
+  const handleDelete = () => {
+    deleteBooking(booking._id)
+      .then(() => {
+          removeBooking(booking._id)
+      })
+  };
+  
+  return (
+    <>
+    <p>Customer name: {booking.name}</p>
+    <p>Customer email: {booking.email}</p>
+    <p>Checked in? {booking.checkedIn}</p>
+    <button onClick={() => handleDelete()}> 🗑 </button>
+    <hr />
+    </>
+  )
 };
 
 export default BookingCard;
