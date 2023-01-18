878f65aead509a06f3b42e3815d03c79274d5553
saladinzero <117020385+saladinzero@users.noreply.github.com>
2023-01-18 13:08:37 +0000

    delete added

--- client/src/BookingCard.js
+++ client/src/BookingCard.js
@@ -1,7 +1,11 @@
 const BookingCard = ({booking, removeBooking}) => {
     return (
         <>
-        <p>This is a BookingCard</p>
+        <p>Customer name: {booking.name}</p>
+        <p>Customer email: {booking.email}</p>
+        <p>Checked in? {booking.checkedIn}</p>
+        <button onClick={() => removeBooking(booking._id)}> 🗑 </button>
+
         </>
     )
 };
